036231ddb0d2971cf2c99c42fad3f961674c3b48
Toxagin <toxagin@gmail.com>
2016-12-26 19:54:32 +0300

    Create decomposition of complex functions, delete unnecessary comments

--- coursera.py
+++ coursera.py
@@ -8,9 +8,11 @@ import json
 import datetime
 import re
 
+DAYS_PER_WEEK = 7
 
 def get_courses_list(courses_count=20):
-    response = requests.get('https://www.coursera.org/sitemap~www~courses.xml')
+    coursera_url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    response = requests.get(coursera_url)
     root = etree.fromstring(response.content)
 
     return random.sample(
@@ -23,16 +25,36 @@ def get_course_info(course_url):
     course_data = {}
     response = requests.get(course_url)
     html_doc = BeautifulSoup(response.content, 'html.parser')
-    api_template = 'https://api.coursera.org/api/courses.v1?q=slug&'\
-        'slug={slug}&fields=startDate'
+    course_data['course_url'] = course_url
+    course_data['title'] = get_title(html_doc)
+    course_data['language'] = get_language(html_doc)
+    starts, commitment = get_start_date_and_commitment(html_doc, course_url)
+    course_data['starts'] = starts
+    course_data['commitment'] = commitment
+    course_data['rating'] = get_rating(html_doc)
+    return course_data
 
-    title = get_by_class(html_doc, 'title display-3-text')
-    language = get_by_class(html_doc, 'language-info')
+
+def get_title(html_doc):
+    result_tag = html_doc.find(class_='title display-3-text')
+    title = None
+    if result_tag:
+        title = result_tag.string
+    return title
+
+
+def get_language(html_doc):
+    result_tag = html_doc.find(class_='language-info')
+    language = None
+    if result_tag and result_tag.contents:
+        language = result_tag.contents[1]
+    return language
+
+
+def get_start_date_and_commitment(html_doc, course_url):
     starts = None
     commitment = 0
-    
-    # try to get course info from html
-    course_info = html_doc.find('script',type='application/ld+json')
+    course_info = html_doc.find('script', type='application/ld+json')
     if course_info:
         course = json.loads(course_info.string)['hasCourseInstance'][0]
         start_date = course.get('startDate')
@@ -48,36 +70,45 @@ def get_course_info(course_url):
                     '%Y-%m-%d'
                 )
                 delta = end_date - start_date
-                commitment = delta.days / 7
+                commitment = delta.days / DAYS_PER_WEEK
             starts = start_date
-    else: # no json in html? try to use api
-        match = re.search(r'/[^/]+$', course_url)
-        slug = match.group(0)[1:]
-        response = requests.get(api_template.format(slug=slug))
-        course = response.json()['elements'][0]
-        start_date = course.get('startDate')
-        if start_date:
-            starts = datetime.datetime.fromtimestamp(int(start_date)/1000)
+    else:
+        starts = get_start_date_from_api(course_url)
 
-    str_rating = get_by_class(html_doc, 'ratings-text bt3-visible-xs')
-    rating = float(str_rating[:4]) if str_rating else 0
-
-    course_data['course_url'] = course_url
-    course_data['title'] = title
-    course_data['language'] = language
-    course_data['starts'] = starts
-    course_data['commitment'] = commitment
-    course_data['rating'] = rating
-    return course_data
+    return starts, commitment        
 
 
-def get_by_class(html_doc, class_info):
-    result = html_doc.find(class_=class_info)
-    if not result:
-        return None
-    elif not result.string and result.contents:
-        return result.contents[1]
-    return result.string
+def get_start_date_from_api(course_url):
+    starts = None
+    api_url = 'https://api.coursera.org/api/courses.v1'
+    match = re.search(r'/[^/]+$', course_url)
+    slug = match.group(0)[1:]
+    params = {
+        'q': 'slug',
+        'slug': slug,
+        'fields': 'startDate'
+    }
+    response = requests.get(api_url, params=params)
+    course = response.json()['elements'][0]
+    start_date = course.get('startDate')
+    if start_date:
+        starts = datetime.datetime.fromtimestamp(
+            convert_to_seconds(int(start_date))
+        )
+    return starts
+
+
+def convert_to_seconds(milliseconds_timestamp):
+    milliseconds_in_second = 1000
+    return milliseconds_timestamp / milliseconds_in_second
+
+
+def get_rating(html_doc):
+    result_tag = html_doc.find(class_='ratings-text bt3-visible-xs')
+    str_rating = ''
+    if result_tag:
+        str_rating = result_tag.string
+    return float(str_rating[:4]) if str_rating else 0
 
 
 def output_courses_info_to_xlsx(courses, filepath):
@@ -98,7 +129,7 @@ def output_courses_info_to_xlsx(courses, filepath):
 
 if __name__ == '__main__':
     output_courses_info_to_xlsx(
-        (get_course_info(course_url) for course_url in get_courses_list(20)),
+        (get_course_info(course_url) for course_url in get_courses_list()),
         input('Enter filepath (.xlsx):')
     )
         
